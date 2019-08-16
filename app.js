919c073db203f2d403dca9d5bb95b6819428bcd1
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-16 13:35:25 -0500

    tutorial is finished but need to fix several things

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector(".temperature-description");
     let temperatureDegree = document.querySelector(".temperature-degree")
     let locationTimezone = document.querySelector(".location-timezone")
+    let temperatureSection = document.querySelector('.temperature-section')
+    const temperatureSpan = document.querySelector('.temperature-section span')
 
 
     if (navigator.geolocation) {
@@ -22,16 +24,39 @@ window.addEventListener('load', () => {
                     console.log(data)
                     const temperature = data.main.temp;
                     const summary = data.weather[0].description;
-                    const location = `${data.name}, ${data.sys.country}`
+                    const location = `${data.name}, ${data.sys.country}`;
+                    const icon = "partly-cloudy-night";
                     // set DOM elements from the API
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = location;
+
+                    //formula for celsius
+                    let celsius = (temperature - 32) * (5 / 9);
+
+                    //set icon
+                    setIcons(icon, document.querySelector(".icon"));
+
+                    //Change temperature to celsius
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === 'F') {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    })
                 })
             })
 
         
     } 
 
-    
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 })
